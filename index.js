2911ed5ab6d7e527e8457efc2f1a289fd743a246
Amit biswas <amitbiswas9282@gmail.com>
2025-01-26 23:08:13 +0600

    last Sign In Time

--- index.js
+++ index.js
@@ -70,6 +70,19 @@ async function run() {
       res.send(result);
     });
 
+    // Patch (last signIn Time)
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req?.body?.lastSignInTime,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
     // Delete Card/id
     app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
@@ -98,7 +111,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
-      res.send(result); 
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
